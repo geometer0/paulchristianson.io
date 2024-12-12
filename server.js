ecd7ff521eb30552c905f19bce11df981035488c
Paul Christianson <pacdeveloping@gmail.com>
2024-12-12 14:10:50 -0500

    css and image files now sending too

--- server.js
+++ server.js
@@ -4,15 +4,40 @@ const path = require('path');
 const url = require('url');
 
 let server = http.createServer((req,res) => {
-    let clientPath = url.parse(req.url).pathname;
-    let status = 404;
-    let serverPath = './html/404.html';
-    console.log("Client requested: ",clientPath) 
-    if (clientPath == '/') {
-        status = 200;
-        serverPath = './html/index.html'
+    const clientPath = url.parse(req.url).pathname;
+    const clientExtension = path.extname(clientPath);
+    const clientFile = path.basename(clientPath);
+    console.log("Client requested: ",clientPath)
+
+    const extensions = {
+        '.html': 'text/html',
+        '.css': 'text/css',
+        '.jpg': 'image/jpg',
     }
-    res.writeHead(status, {'Content-Type': 'text/html'});
+
+    const dirs = {
+        '.html': 'html',
+        '.css': 'css',
+        '.jpg': 'img',
+    }
+
+    let serverPath = './html/404.html';
+    let status = 404;
+    
+    if (clientPath == '/') {
+        serverPath = './html/index.html'
+        status = 200;
+    } else if (clientExtension in dirs) {
+        console.log("Hi, you've reached clientExtension in dirs")
+        console.log("directory found: ",dirs[clientExtension])
+        serverPath = path.join(__dirname, dirs[clientExtension], clientFile)
+        console.log("Serverpath set to ",serverPath)
+        status = 200;
+        //once this works add check if file exists otherwise 404
+    }
+
+    res.writeHead(status, {'Content-Type': extensions[clientExtension] || 'text/html'} );
+    
     fs.createReadStream(serverPath).pipe(res);
 });
 server.listen(8080)
